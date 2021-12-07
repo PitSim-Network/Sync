ca6c15e376593ced5e53d1dca6797278afdd2c4a
KyroKrypt <kyrokrypt@gmail.com>
2021-12-06 18:23:50 -0800

    minor

--- src/main/java/net/pitsim/sync/enums/PvpArena.java
+++ src/main/java/net/pitsim/sync/enums/PvpArena.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public enum PvpArena {
-
 	JUNGLE("Jungle", "plugins/WorldEdit/schematics/jungle.schematic", new Location(Bukkit.getWorld("pvp"), 44, 1, 0, 0, 90),
 			new Location(Bukkit.getWorld("pvp"), -36, 2, 0, 0, -90)),
 	SWAMP("Swamp", "plugins/WorldEdit/schematics/swamp.schematic", new Location(Bukkit.getWorld("pvp"), 36, 2, 0, 0, 90),
@@ -14,12 +13,10 @@ public enum PvpArena {
 	DESERT("Desert", "plugins/WorldEdit/schematics/desert.schematic", new Location(Bukkit.getWorld("pvp"), 31, 0, 0, 0, 90),
 			new Location(Bukkit.getWorld("pvp"), -29, -1, 0, 0, -90));
 
-
 	public String refName;
 	public String schematicName;
 	public Location player1Spawn;
 	public Location player2Spawn;
-
 
 	PvpArena(String refName, String schematicName, Location player1Spawn, Location player2Spawn) {
 		this.refName = refName;
