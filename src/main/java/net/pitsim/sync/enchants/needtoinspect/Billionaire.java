ecf1708d267a79a1295e7b84a4a46d6bb7fcf422
KyroKrypt <kyrokrypt@gmail.com>
2021-12-29 12:56:25 -0800

    change bill to damage increase

--- src/main/java/net/pitsim/sync/enchants/needtoinspect/Billionaire.java
+++ src/main/java/net/pitsim/sync/enchants/needtoinspect/Billionaire.java
@@ -24,29 +24,24 @@ public class Billionaire extends PitEnchant {
 		int enchantLvl = attackEvent.getAttackerEnchantLevel(this);
 		if(enchantLvl == 0) return;
 
-		attackEvent.multiplier.add(getDamageMultiplier(enchantLvl));
-//		attackEvent.increasePercent += getDamageIncrease(enchantLvl) / 100.0;
+//		attackEvent.multiplier.add(getDamageMultiplier(enchantLvl));
+		attackEvent.increasePercent += getDamageIncrease(enchantLvl) / 100.0;
 		Sounds.BILLIONAIRE.play(attackEvent.attacker);
 	}
 
 	@Override
 	public List<String> getDescription(int enchantLvl) {
-		DecimalFormat decimalFormat = new DecimalFormat("0.##");
-		return new ALoreBuilder("&7Hits with this sword deal &c" + getDamageMultiplier(enchantLvl) + "x",
-				"&cdamage &7but cost &6" + getGoldCost(enchantLvl) + "g").getLore();
+		DecimalFormat decimalFormat = new DecimalFormat("0.#");
+		return new ALoreBuilder("&7Hits with this sword deal",
+				"&c+" + decimalFormat.format(getDamageIncrease(enchantLvl)) + "% damage &7but cost &6" + getGoldCost(enchantLvl) + "g").getLore();
 	}
 
-//	public double getDamageIncrease(int enchantLvl) {
-//		if(enchantLvl % 3 == 0) return enchantLvl;
-//		return (enchantLvl / 3.0) * 100;
-//	}
-
-	public double getDamageMultiplier(int enchantLvl) {
-		return (double) Math.round((1 + (double) enchantLvl / 3) * 100) / 100;
+	public double getDamageIncrease(int enchantLvl) {
+		if(enchantLvl % 3 == 0) return (double) (enchantLvl / 3) * 100;
+		return (enchantLvl / 3.0) * 100;
 	}
 
 	public int getGoldCost(int enchantLvl) {
-
 		return (int) (Math.floor(Math.pow(enchantLvl, 1.75)) * 50 + 50);
 	}
 }
