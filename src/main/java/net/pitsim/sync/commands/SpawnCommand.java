296e2c1f61bf7e28e7d079d20a65b453e5e6c453
KyroKrypt <kyrokrypt@gmail.com>
2021-12-29 12:49:17 -0800

    re-routing /oof message

--- src/main/java/net/pitsim/sync/commands/SpawnCommand.java
+++ src/main/java/net/pitsim/sync/commands/SpawnCommand.java
@@ -1,5 +1,6 @@
 package net.pitsim.sync.commands;
 
+import dev.kyro.arcticapi.misc.AOutput;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,6 +8,7 @@ import org.bukkit.command.CommandSender;
 public class SpawnCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        AOutput.send(sender, "&7/spawn will act like /oof temporarily");
         OofCommand.INSTANCE.onCommand(sender, cmd, label, args);
 
 //        Player player = (Player) sender;
