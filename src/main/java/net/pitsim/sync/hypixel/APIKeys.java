8c1d92bd95509ba950071615ef18f86945b1fce1
wiji <wijigaming.yt@gmail.com>
2023-06-18 22:46:19 -0700

    Moved API key to config file Updated default config

--- src/main/java/net/pitsim/sync/hypixel/APIKeys.java
+++ src/main/java/net/pitsim/sync/hypixel/APIKeys.java
@@ -1,16 +1,9 @@
 package net.pitsim.sync.hypixel;
 
-import net.pitsim.sync.misc.Misc;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import dev.kyro.arcticapi.data.AConfig;
 
 public class APIKeys {
 	public static String getAPIKey() {
-		return "***REMOVED***";
+		return AConfig.getString("hypixel-api-key");
 	}
 }
